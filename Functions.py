703b323e96f9a6cc1a72ef0fc4d0e37ea47f26bb
omarlopez2001 <omarlopez9445@gmail.com>
2020-09-22 11:56:14 -0400

    Create Functions.py
    Functions.py file with solutions

---
+++ Functions.py
@@ -0,0 +1,25 @@
+def area(width, height):
+    result = width * height
+    return result
+result = area(5,6)
+print(result)
+result = area(7, 9)
+print(result)
+result = area(12, 12)
+print(result)
+
+def subtract(num1, num2):
+    result = num1 - num2
+    return result
+result = subtract(20, 10)
+print(result)
+result = subtract(50, 30)
+print(result)
+
+def add(num1, num2):
+    result = num1 + num2
+    return result
+result = add(10, 2)
+print(result)
+result = add(100, 5)
+print(result)
